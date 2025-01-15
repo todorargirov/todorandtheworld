8c7ee1c6cd4b670cfa155129c563503447c40a24
Epiprocta <epiprocta@outlook.com>
2025-01-15 22:05:20 +0800

    fixup

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,11 +1,22 @@
 "use client";
 
 import { Chip } from "@mui/material";
+import { createTheme, ThemeProvider } from "@mui/material/styles";
+import { CssBaseline } from "@mui/material";
+
+const darkTheme = createTheme({
+    palette: {
+        mode: "dark",
+    },
+});
 
 export default function Home() {
     return (
-        <div>
-            <Chip label={"Todor And The World"} />
-        </div>
+        <ThemeProvider theme={darkTheme}>
+            <CssBaseline />
+            <div>
+                <Chip label={"Todor And The World"} />
+            </div>
+        </ThemeProvider>
     );
 }
